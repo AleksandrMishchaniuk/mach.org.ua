398b2336e68fb1d0e233b3fe98d4cbd90417f0a5
AleksandrMishchaniuk <map.m@ua.fm>
2018-06-02 17:13:13 +0300

    added gost download link

--- src/pages/download.vue
+++ src/pages/download.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page padding>
-    <h2>Downloads</h2>
+    <q-list>
+      <q-item>
+        <a href="http://dopusk.homestead/downloads/GOST-25347-82.pdf" download="GOST-25347-82.pdf" target="_blank">ГОСТ 25347-82.pdf</a>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
